c3054a03fb858322cd51849ff293160fb428c28b
Antoine MOHR <antoine.mohr@viacesi.fr>
2019-09-23 14:19:50 +0200

    fonctions vue

--- src/View/View.java
+++ src/View/View.java
@@ -4,6 +4,10 @@ import Controller.Controller;
 
 public class View {
 	private Controller controller;
+	
+	public static int SUCCESS = 1;
+	public static int FAIL = 2;
+	
 
 	public View() {
 		Frame frame = new Frame();
@@ -13,4 +17,43 @@ public class View {
 	public void setController(Controller controller) {
 		this.controller = controller;
 	}
+	
+	
+	public void displayMessage(String message, int state) {
+		if(state == SUCCESS) {
+			
+		}else if (state == FAIL) {
+			
+		}
+	}
+	
+	/* Auth */ 
+	public String getLogin() {
+		return null;
+	}
+	
+	public String getPassword() {
+		return null;
+	}
+	
+	
+	public void connexionSuccessful() {
+		
+	}
+	
+	public void connexionFailed() {
+		
+	}
+	
+	
+	/* 1st window */
+	public String getSourcePath() {
+		return null;
+	}
+	
+	public String getDestinationPath() {
+		return null;
+	}
+	
+	
 }
