f0a5658ac556ce205c828911bf243c73ee59808d
N6nico <nicolas.ekobe@viacesi.fr>
2019-09-23 11:01:45 +0200

    fonction activer lors d'une demande de co + checker de correspondance login mdp

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,8 +4,44 @@ import Model.Model;
 import View.View;
 
 public class Controller {
+	private Model model;
+	private View view;
+
 	public Controller(Model model, View view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	public void wantToConnect() {
+		//isConnected is the returned attribute
+		Boolean isConnected = new Boolean(isConnected);
 		
+		//Search the login and password in the view
+		isConnected = this.pcs_authentifier(view.getLogin(), view.getPassword());
+		
+		//Tell the View if the user is connected or not
+		if(isConnected) {
+			view.isConnected(view.getLogin());
+		}
+		else {
+			view.isntConnected();
+		}
+	}
+	
+	public Boolean pcs_authentifier(String login, String password) {
+		//Check if this login + password exist
+		if(model.selectIDbyLoginPassword(login, password) != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public Boolean pcs_decrypter(String source_path, String destination_path) {
+		return true;
 	}
 
 }
+
+
