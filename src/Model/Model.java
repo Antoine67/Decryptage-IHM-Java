700c1c48165acb7acc999ea50fe2ff40f262de6d
Antoine MOHR <antoine.mohr@viacesi.fr>
2019-09-23 11:23:34 +0200

    fonctions modèle màj

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -19,5 +19,47 @@ public class Model {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	
+	 /* Composant d’accès aux données [CAD] */
+
+	ArrayList<User> getRows(String rq_sql, String resultSetName) throws SQLException {
+		return databaseDAO.getRows(rq_sql, resultSetName);
+	}
+
+	void actionRows(String rq_sql) /* – Optionnel */ {
+		databaseDAO.actionRows(rq_sql);
+	}
+
+	/* Composant de manipulation de fichiers [Files] */
+
+	String getData(String path) {
+		return null; //TODO
+	}
+
+	void setData(String path) {
+		
+	}
+
+	/* Composant de décryptage [Decrypt] */
+
+	String decrypt(String data, String key) {
+		return null; //TODO
+	}
+
+	/* Composant de mappage de la table personne [Map_P] */
+
+	String selectIDbyLoginPassword(String login, String password) throws SQLException {
+		return databaseDAO.selectIDbyLoginPassword(login, password);
+	}
+
+	/* Composant de mappage de la table dictionnaire [Map_Dic] */
+
+	String selectWord (String word) {
+		return null; // TODO
+	}
+	
+	
 
 }
