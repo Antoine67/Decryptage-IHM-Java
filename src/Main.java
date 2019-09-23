e73ac3460c7ed81dbab83e99a20dc9ce991c26cf
Antoine MOHR <antoine.mohr@viacesi.fr>
2019-09-23 15:32:33 +0200

    màj main

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,14 @@ import View.View;
  
 public class Main {
   public static void main(String[] args) throws SQLException{
+	  
+	  Controller controller = new Controller();
+	  
 	  Model model = new Model();
-	  View view = new View();
-	  Controller controller = new Controller(model, view);
-	  view.setController(controller);
+	  View view = new View(controller);
+	  
+	  controller.setModelAndView(model, view);
+
 	  
   }       
 }
