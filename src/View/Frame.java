56813db39da0e76226c5c63855c6416040113907
Admin <Admin@DESKTOP-4M92FGJ>
2019-09-23 12:20:33 +0200

    view update

--- src/View/Frame.java
+++ src/View/Frame.java
@@ -1,15 +1,20 @@
 package View;
+
+import java.awt.Color;
+
 import javax.swing.JFrame;
+
 
 public class Frame extends JFrame {
 
-	  public Frame(){                
-	    this.setTitle("Interface de connexion utilisateur");
-	    this.setSize(500, 200);
-	    this.setLocationRelativeTo(null);               
+	  public Frame(){
+		this.setTitle("MadMax");
+		this.setSize(400, 500);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setLocationRelativeTo(null);
+	    this.setBackground(Color.white);
 	    this.setContentPane(new Panel());
 	    this.setVisible(true);
-	  }     
-	
+	  }       
+
 }
--- src/View/Panel.java
+++ src/View/Panel.java
@@ -1,11 +1,70 @@
 package View;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.text.NumberFormat;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Panel extends JPanel {
+	  private JFormattedTextField login = new JFormattedTextField(NumberFormat.getIntegerInstance());
+	  private JFormattedTextField password = new JFormattedTextField(NumberFormat.getPercentInstance());
+	  private JLabel loginlab = new JLabel("Login");
+	  private JLabel passwordlab = new JLabel("Password");
+	  private JButton signin = new JButton ("Sign In");
+	  private JPanel thisainer = new JPanel();
+	  
 	public void paintComponent(Graphics g){
-		g.drawString("test", 10, 20);
-	}     
+		
+		try {
+		      Image img = ImageIO.read(new File("D:\\eclipse-workspace\\GUI\\src\\images\\cesi.png"));
+		      g.drawImage(img, 100, 50, this);
+		    } 
+		catch (IOException e) {
+		      e.printStackTrace();
+		    }
+		
+	    Font police = new Font("Arial", Font.BOLD, 14);
+	    
+	    login.setFont(police);
+	    login.setPreferredSize(new Dimension(150, 30));
+	    
+	    password.setPreferredSize(new Dimension(150, 30));
+	    
+	    signin.addActionListener(new BoutonListener());
+	    
+	    this.add(loginlab);
+	    this.add(login);
+	    this.add(passwordlab);
+	    this.add(password);
+	    this.add(signin);
+	    this.setBackground(Color.white);
+	    this.setVisible(true); 
+	}
+	
+	
+	
+	  class BoutonListener implements ActionListener{
+		    public void actionPerformed(ActionEvent e) {
+		      System.out.println("Login : " + login.getText());
+		      System.out.println("Password : " + password.getText());
+		    }
+		  }
+	  
+	  
+	  
+	  
 }
 
 
