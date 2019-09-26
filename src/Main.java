d02122afd46ceb4fed68763dd8fb8e7332a8a481
Guiguiprog <33095251+Guiguiprog@users.noreply.github.com>
2019-09-26 22:02:51 +0200

    Improved code reading of the class main

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,29 @@
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.sql.SQLException;
-
 import javax.swing.JFrame;
-
 import Controller.Controller;
 import Model.Model;
 import View.View;
- 
+
+//Start of the program
 public class Main {
-  public static void main(String[] args) throws SQLException, InterruptedException{
-	  
-	  Controller controller = new Controller();
-	  
-	  Model model = new Model();
-	  
-	  View view = new View(controller);
-	  
-	  
+	public static void main(String[] args) throws SQLException, InterruptedException {
 
-	 
-          
-	  
-	  controller.setModelAndView(model, view);
-	  
-	  
-  
-  }   
+		// We instantiate the different parts of the MVC pattern
 
+		// New instance of the controller
+		Controller controller = new Controller();
 
-  
-   
-  
-  
-  
-  
-  
-  
-  
-  
+		// New instance of the Model
+		Model model = new Model();
+
+		// New instance of the view
+		View view = new View(controller);
+
+		// We send the view and the model to the controller
+		controller.setModelAndView(model, view);
+
+	}
+
 }
