4c3d8a32c8c1f835dd090e463e6142d60f26b02d
Guiguiprog <33095251+Guiguiprog@users.noreply.github.com>
2019-09-26 22:19:06 +0200

    Improved code reading of the decrypter class

--- src/Controller/Decrypter.java
+++ src/Controller/Decrypter.java
@@ -11,16 +11,19 @@ import javax.swing.JProgressBar;
 import View.SelectPanel;
 
 public class Decrypter {
+
 	Controller controller;
+
+	// constructor of the decrypter class
 	public Decrypter(Controller controller) {
 		this.controller = controller;
 
 	}
-	
+
+	// Here we enter the maximum size of the key
 	public static int MAX_KEY_LENGHT = 12;
-	
+
+	// We declare a variable that gives us the time of the keys
 	String temp_key;
 
-	
-		 
 }
