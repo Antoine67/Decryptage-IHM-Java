8ffcfb7a7e734e8ca3676722264218f87a7f3339
Guiguiprog <33095251+Guiguiprog@users.noreply.github.com>
2019-09-26 22:59:33 +0200

    Indent code and butify

--- src/View/Frame.java
+++ src/View/Frame.java
@@ -5,32 +5,41 @@ import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 public class Frame extends JFrame {
 
-	
 	public Panel currentPanel;
 	private View view;
-	
-	
-	
-	
-	  public Frame(View view){
-		  
-		this.view = view;
-		currentPanel = new LoginPanel(view, this);  
-		  
-		this.setTitle("MadMax");
-		this.setSize(400, 500);
-	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    this.setLocationRelativeTo(null);
-	    this.setBackground(Color.white);
-	    this.setContentPane(currentPanel);
-	    this.setVisible(true);
-	    this.setResizable(false);
-	    
-	  }
 
+	public Frame(View view) {
+
+		this.view = view;
+		currentPanel = new LoginPanel(view, this);
+		
+		//set the title of the frame
+		this.setTitle("MadMax");
+		
+		//set the size of the frame
+		this.setSize(400, 500);
+		
+		//shutdown the program when the frame is closed
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//default location
+		this.setLocationRelativeTo(null);
+		
+		//background color
+		this.setBackground(Color.white);
+		
+		//select the current panel
+		this.setContentPane(currentPanel);
+		
+		//makes the frame visible
+		this.setVisible(true);
+		
+		//remove the resizable propertie
+		this.setResizable(false);
+
+	}
 
 	public Panel getCurrentPanel() {
 		return currentPanel;
@@ -40,16 +49,14 @@ public class Frame extends JFrame {
 		this.currentPanel = currentPanel;
 	}
 
-
 	public void connectionSuccessful() {
-		this.setSize(1100,800);
+		
+		//set the size of the new frame
+		this.setSize(1100, 800);
 		this.currentPanel = new SelectPanel(view, this);
 		setContentPane(currentPanel);
 		setLocationRelativeTo(null);
-		
-		
-	}       
-	  
-	  
+
+	}
 
 }
